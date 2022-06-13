17834049a322aa2cfea115ff7298727b412ab60f
Shy L <63942346+ShyLee873@users.noreply.github.com>
2022-06-13 18:01:35 -0400

    answers for lesson 1

---
+++ 01-node-tutorial/answers/answers/1-intro.js
@@ -0,0 +1,12 @@
+const amount = 9
+
+if (amount < 10){
+    console.log('small number');
+}
+else{
+    console.log('large number');
+}
+console.log(`hey, it's my first node app!!`);
+
+//render with node 1-intro.js in terminal
+
